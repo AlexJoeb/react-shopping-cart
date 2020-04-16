e60a497eb9c66215789ccc8740889a39e16a3cae
Alexander Besse <alexanderjoeb@hotmail.com>
2020-04-16 18:12:02 -0500

    Completed with stretch.

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Components
 import Item from './ShoppingCartItem';
 
+import CartContext from '../context/CartContext';
+
 const ShoppingCart = props => {
-	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+
+	const { cart } = useContext(CartContext);
+	
+	const getCartTotal = cart => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
-			))}
+			{cart.map(item => (
+					<Item key={item.id} {...item} />
+				))}
 
 			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
+				<p>Total: ${getCartTotal(cart)}</p>
 				<button>Checkout</button>
 			</div>
 		</div>
